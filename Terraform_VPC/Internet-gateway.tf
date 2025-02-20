4dcb06dc947ddb74f773535beb47d6c6a19d139f
Sai9700128 <burra.sa@northeastern.edu>
2025-02-19 20:27:11 -0500

    added internet gateway

---
+++ Terraform_VPC/Internet-gateway.tf
@@ -0,0 +1,10 @@
+
+
+# creating Internet Gateway
+
+resource "aws_internet_gateway" "public_internet_gateway"{
+    vpc_id = aws_vpc.tf-aws-vpc.id
+    tags = {
+        Name = "IGW: For tf-aws-vpc"
+    }
+}
