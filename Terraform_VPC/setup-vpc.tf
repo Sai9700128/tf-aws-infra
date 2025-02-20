98db9b087a82f3768ee586f4cc10a6e1371d88d5
Sai9700128 <burra.sa@northeastern.edu>
2025-02-19 20:28:06 -0500

    added vpc setup

---
+++ Terraform_VPC/setup-vpc.tf
@@ -0,0 +1,19 @@
+# Cloud Provider Details
+
+provider "aws" {
+    profile = var.profile
+    region = var.region
+}
+
+
+# Create AWS VPC in us-east-1
+
+resource "aws_vpc" "tf-aws-vpc"{
+    cidr_block = var.vpc_cidr
+    tags = {
+        Name = "tf-aws-vpc"
+    }
+}
+
+
+
