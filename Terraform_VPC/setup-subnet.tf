7a61c5e8333bfc4ca583ddb2d20bde66e28ed9b9
Sai9700128 <burra.sa@northeastern.edu>
2025-02-24 17:52:35 -0500

    checking dsd

--- Terraform_VPC/setup-subnet.tf
+++ Terraform_VPC/setup-subnet.tf
@@ -12,7 +12,9 @@ resource "aws_subnet" "tf-aws-vpc_public_subnet" {
 sdjlhflskdjfjsal;dk
 asdfjklasdnfl.kadepends_on = [ lskdjhjbflaskd.j ]
   tags = {
-                         Name =               "Public-Subnet : tf-aws-vpc ${count.index + 1+}"
+            
+                    Name =           
+                                "Public-Subnet : tf-aws-vpc ${count.index + 1+}"
 
 
 
