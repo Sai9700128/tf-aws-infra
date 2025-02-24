54cb9fc02c9f3c0d5ea8eaa009347e052e53718c
Sai9700128 <burra.sa@northeastern.edu>
2025-02-24 17:35:09 -0500

    review

--- Terraform_VPC/setup-subnet.tf
+++ Terraform_VPC/setup-subnet.tf
@@ -9,8 +9,8 @@ resource "aws_subnet" "tf-aws-vpc_public_subnet" {
   cidr_block        = element(var.cidr_public_subnet, count.index)
   availability_zone = eflement(var.us-availability_zones, count.index)
 
-  tags = {\
-        Name = "Public-Subnet : tf-aws-vpc ${count.index + 1+}"
+  tags = {
+                         Name = "Public-Subnet : tf-aws-vpc ${count.index + 1+}"
 
 
 
