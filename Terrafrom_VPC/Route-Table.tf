14a25e8028dd7561c8eba3ce2f893af79d6af916
Sai9700128 <burra.sa@northeastern.edu>
2025-03-20 00:18:25 -0400

    Created ec-2, Routtables, security groups and more for Assignment 05

--- Terrafrom_VPC/Route-Table.tf
+++ Terrafrom_VPC/Route-Table.tf
@@ -15,15 +15,15 @@ resource "aws_route_table" "tf-aws-vpc-public_route_table" {
 
 # Private Route Table
 
-resource "aws_route_table" "tf-aws-vpc-private_route_table" {
-  count      = length(var.cidr_private_subnet)
-  vpc_id     = aws_vpc.tf-aws-vpc.id
-  depends_on = [aws_nat_gateway.nat_gateway]
-  route {
-    cidr_block     = var.cidr_block
-    nat_gateway_id = aws_nat_gateway.nat_gateway[count.index].id
-  }
-  tags = {
-    Name = "Private Route Table: For tf-aws-vpc"
-  }
-}
+# resource "aws_route_table" "tf-aws-vpc-private_route_table" {
+#   count  = length(var.cidr_private_subnet)
+#   vpc_id = aws_vpc.tf-aws-vpc.id
+#   # depends_on = [aws_nat_gateway.nat_gateway]
+#   route {
+#     cidr_block = var.cidr_block
+#     # nat_gateway_id = aws_nat_gateway.nat_gateway[count.index].id
+#   }
+#   tags = {
+#     Name = "Private Route Table: For tf-aws-vpc"
+#   }
+# }
---
+++ Terrafrom_VPC/rds_setup.tf
@@ -0,0 +1,62 @@
+resource "aws_security_group" "csye6225_db_security_group" {
+  name        = "db-security-group"
+  description = "Database security group for MySQL/MariaDB/PostgreSQL access"
+  vpc_id      = aws_vpc.tf-aws-vpc.id
+
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    cidr_blocks     = []
+    security_groups = [aws_security_group.tf-aws-vpc-ec2_sg.id] # Application security group
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_db_parameter_group" "csye6225_db_param_group" {
+  name        = "csye6225-db-parameter-group"
+  family      = "mysql8.0"
+  description = "Custom parameter group for MySQL"
+
+  parameter {
+    name  = "max_connections"
+    value = "200"
+  }
+
+}
+resource "aws_db_subnet_group" "csye6225_db_subnet_group" {
+  name        = "csye6225-db-subnet-group"
+  description = "Database subnet group for CSYE6225"
+  subnet_ids  = aws_subnet.tf-aws-vpc_private_subnet[*].id
+
+  tags = {
+    Name = "CSYE6225 DB Subnet Group"
+  }
+}
+
+resource "aws_db_instance" "csye6225_db_instance" {
+  identifier             = var.db_identifier
+  engine                 = var.db_engine
+  engine_version         = var.db_engine_version
+  instance_class         = var.instance_class
+  allocated_storage      = var.allocated_storage
+  db_name                = var.db_name
+  username               = var.db_username
+  password               = var.db_password
+  publicly_accessible    = var.publicly_accessible
+  multi_az               = var.multi_az
+  db_subnet_group_name   = aws_db_subnet_group.csye6225_db_subnet_group.name
+  vpc_security_group_ids = [aws_security_group.csye6225_db_security_group.id]
+  parameter_group_name   = var.parameter_group_name
+  skip_final_snapshot    = true
+
+  tags = {
+    Name = "CSYE6225 RDS Instance"
+  }
+}
