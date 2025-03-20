609baf6b7a800f0cf86b7db8b30e0efa48240e3f
Sai9700128 <burra.sa@northeastern.edu>
2025-03-20 00:20:36 -0400

    formatted

--- Terrafrom_VPC/S3_bucket.tf
+++ Terrafrom_VPC/S3_bucket.tf
@@ -4,7 +4,7 @@ resource "random_uuid" "bucket_name" {}
 
 # Creating a private S3 bucket  with the random UUID generated
 resource "aws_s3_bucket" "private_bucket" {
-  bucket        = random_uuid.bucket_name.result
+  bucket = random_uuid.bucket_name.result
   # Ensures Terraform can delete the bucket even if not empty
   force_destroy = true
 
@@ -18,11 +18,11 @@ resource "aws_s3_bucket_public_access_block" "block_public" {
   bucket = aws_s3_bucket.private_bucket.id
 
   # Blocks new public ACLs from being added
-  block_public_acls       = true
+  block_public_acls = true
   # Prevents the bucket from having a public policy
-  block_public_policy     = true
+  block_public_policy = true
   # Ignores any existing public ACLs
-  ignore_public_acls      = true
+  ignore_public_acls = true
   # Blocks all public access, even if ACLs and policies are misconfigured
   restrict_public_buckets = true
 }
@@ -40,7 +40,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
 resource "aws_s3_bucket_lifecycle_configuration" "lifecycle_policy" {
   bucket = aws_s3_bucket.private_bucket.id
 
-#   Rule for moving objects to IA storage class after 30 days
+  #   Rule for moving objects to IA storage class after 30 days
   rule {
     id     = "move-to-IA"
     status = "Enabled"
